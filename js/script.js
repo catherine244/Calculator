de14903326666cc6d0f92cd45dd470a435923788
catherine244 <ccayreen24@gmail.com>
2019-10-24 10:17:42 +0300

    changed function to multply

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var subtract = function(number1, number2) {
-    return number1 - number2;
+var multply = function(number1, number2) {
+    return number1 * number2;
   };
   
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
-  var result = subtract(number1, number2);
+  var result = multply(number1, number2);
   alert(result);
